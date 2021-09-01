7e06b727a5681e48d81a81bd5607b59d81d1ade9
Gabriel Troian Trevisan <gabrieltr@outlook.com>
2021-09-01 09:48:12 -0300

    Bugfixes

--- lib/esm/index.d.ts
+++ lib/esm/index.d.ts
@@ -1,6 +1,6 @@
 import * as React from "react";
 interface DoneTypingInputProperties extends React.InputHTMLAttributes<HTMLInputElement> {
-    doneTypingTimer: number | 500;
+    doneTypingTimer?: number | 500;
     onDoneTyping: any;
 }
 declare const DoneTypingInput: React.FC<DoneTypingInputProperties>;
